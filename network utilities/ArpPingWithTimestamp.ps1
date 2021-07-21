2ecd7c93a8e54d7a5f218aed4a13293847d26c45
imbrikis <imbrikis@yahoo.com>
2021-07-20 21:41:50 -0600

    arp ping script

--- network utilities/ArpPingWithTimestamp.ps1
+++ network utilities/ArpPingWithTimestamp.ps1
@@ -1,3 +1,5 @@
+# uses nmap to continuously arp ping a target (no port scan option selected) and output to a file (timestamp included) 
+
 while ($true) {
   Start-Sleep -m 2000
   nmap -sn -PR xx.xx.xx.xx | Out-File -FilePath "C:\pingResult.txt" -Append # replace 'xx' with target IP address
